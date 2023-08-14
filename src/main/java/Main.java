c76094f0701f701b157180d5ef599359fe334743
GrishaevaOV <olga.gr2812@mail.ru>
2023-08-14 17:14:24 +0300

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Как тебя зовут?");
+        Scanner in=new Scanner(System.in);
+        String name = in.nextLine();
+        System.out.println("Привет, "+name+"!");
+
     }
 }
